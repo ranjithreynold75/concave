cfecb48770617f7f55dfa83199d224c8ed2420ba
ranjith <ranjithreynold@gmail.com>
2017-03-30 10:39:39 -0700

    intial commit

---
+++ bin/app.js
@@ -0,0 +1,27 @@
+var express=require('express');
+var app=express();
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+
+
+var bodyparser=require('body_parser');
+
+function notify(req,res,next){
+    console.log('request to'+req.url);
+next();
+}
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use (notify);
+
+require('../router/route')(app,io);
+
+
+
+var server=http.listen(process.env.PORT || 8000,function(){
+    console.log("server running in 8000");
+});
+
+
+
+
---
+++ router/route.js
@@ -0,0 +1,36 @@
+
+//mongodb://<dbuser>:<dbpassword>@ds145380.mlab.com:45380/concave
+
+var m=require('mongodb');
+var url="mongodb://concave:alwaysforward1.@ds145380.mlab.com:45380/concave";
+var mc=m.MongoClient;
+var _db;
+mc.connect(url,function(err,db){
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        _db=db;
+
+    }
+});
+
+
+
+
+
+module.exports=function (app,io) {
+
+
+
+
+
+
+
+
+
+
+
+}
