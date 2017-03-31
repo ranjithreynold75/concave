0cecd62727d14482c5f4f359876fbb5170b3c376
ranjith <ranjithreynold@gmail.com>
2017-03-30 17:14:43 -0700

    intial commit

--- bin/app.js
+++ bin/app.js
@@ -2,6 +2,7 @@ var express=require('express');
 var app=express();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
+var classifier=require('classifier');
 var bayes=new classifier.Bayesian();
 
 bayes.train("i like pizza","pizza");
